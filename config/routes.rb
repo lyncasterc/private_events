465288e6f990d34a97884a78a68c6ad117ea28c3
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-08 16:22:00 -0400

    create show route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   resources :events
+  resources :users, only: [:show]
 
   root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
