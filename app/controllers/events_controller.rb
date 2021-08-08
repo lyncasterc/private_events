878d5948ad474ba9504a8e8127d039c18e04be25
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-08 17:48:18 -0400

    write new, create actons for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,30 @@
 class EventsController < ApplicationController
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: "Event created!" }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end 
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date)
   end
 end
