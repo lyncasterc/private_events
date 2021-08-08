cbf2adee4dadc0d98608da58294cda4d117e87f2
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-08 15:53:26 -0400

    add creator_id column to event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_08_194548) do
+ActiveRecord::Schema.define(version: 2021_08_08_195243) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_08_194548) do
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
