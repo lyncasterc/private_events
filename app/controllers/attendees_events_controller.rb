83d5e536b2c42bb713ce2c071d76e970db02b59e
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-09 18:59:52 -0400

    generate attendees_events controller

---
+++ app/controllers/attendees_events_controller.rb
@@ -0,0 +1,2 @@
+class AttendeesEventsController < ApplicationController
+end
---
+++ test/controllers/attendees_events_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AttendeesEventsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
