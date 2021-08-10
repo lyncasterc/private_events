cd3c6a1951ee25f599b206fb1e2d39893a0967d5
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-10 18:46:32 -0400

    create route for attending event

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
-  resources :events
+  
+  resources :events do
+    resources :attendees_events, only: [:new, :create, :destroy]
+  end
   resources :users, only: [:show]
+
+  get '/events/:id/attend', to: 'events#attend'
 
   root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
