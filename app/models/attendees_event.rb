ab54caeab7de778b90711e82f50c354f26bc4a6c
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-10 18:47:27 -0400

    add validation for attendee_id, event_id uniqueness

--- app/models/attendees_event.rb
+++ app/models/attendees_event.rb
@@ -1,4 +1,5 @@
 class AttendeesEvent < ApplicationRecord
+  validates_uniqueness_of :attendee_id, scope: :event_id
   belongs_to :attendee, class_name: 'User'
   belongs_to :event, class_name: 'Event'
 end
