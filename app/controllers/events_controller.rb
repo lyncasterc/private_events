34dd4b08e2a0bb95c5426989eb702632ac752c2f
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-12 21:18:54 -0400

    add authentication on application controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @events = Event.all
