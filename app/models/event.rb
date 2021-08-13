63173a22662e1a1a5c9145056e2a6ed673743332
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-12 21:41:43 -0400

    add image to user and events assocations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendees_events
   has_many :attendees, through: :attendees_events
+  has_one_attached :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
   has_many :attendees_events, foreign_key: :attendee_id
   has_many :attended_events, through: :attendees_events, source: :event
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
