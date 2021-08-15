491826dac1e96857f3a30ee18392a2671a153d3a
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-15 15:19:39 -0400

    add route for delete_user_image

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   resources :events do
     resources :attendees_events, only: [:new, :create, :destroy]
   end
-  resources :users, only: [:show]
 
+  resources :users, only: [:show]
+    
   get '/events/:id/attend', to: 'events#attend'
+  get '/users/edit/delete_user_image', to: 'users#delete_user_image'
+  
 
   root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
