7b389291bf0dce8bb9b495160bca5e44ff99c03a
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-15 15:19:28 -0400

    write delete_user_image controller action in Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,15 @@ class UsersController < ApplicationController
   def show
   end
 
+  def delete_user_image
+    @user_image = ActiveStorage::Attachment.find_by(id: current_user.image.id)
+    @user_image.purge_later
+    redirect_back(fallback_location: root_path)
+  end
+
   private
   
   def set_user
-    @user = User.find(params[:id])
+    @user = current_user
   end
 end
