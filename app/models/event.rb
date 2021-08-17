bccf61e0fb3a0e2037c9c4e30114efc7f1fabc3f
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-17 15:31:26 -0400

    add Event scopes for past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,13 @@
 class Event < ApplicationRecord
+  scope :past, -> { where("date < ?", Date.today) } 
+  scope :upcoming, -> { where("date >= ?", Date.today) }
+  
   belongs_to :creator, class_name: 'User'
   has_many :attendees_events, dependent: :destroy
   has_many :attendees, through: :attendees_events, dependent: :destroy
   has_one_attached :image
 
   validates :title, :description, :date, :time, :location, :image, presence: true
+
+
 end
