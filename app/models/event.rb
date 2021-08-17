35df33f9677296bbcabef6b09e4520810be6dd16
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-16 22:23:21 -0400

    update associations for deletion

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :attendees_events
-  has_many :attendees, through: :attendees_events
+  has_many :attendees_events, dependent: :destroy
+  has_many :attendees, through: :attendees_events, dependent: :destroy
   has_one_attached :image
 
   validates :title, :description, :date, :time, :location, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
-  has_many :attendees_events, foreign_key: :attendee_id
-  has_many :attended_events, through: :attendees_events, source: :event
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event', dependent: :destroy
+  has_many :attendees_events, foreign_key: :attendee_id, dependent: :destroy
+  has_many :attended_events, through: :attendees_events, source: :event, dependent: :destroy
   has_one_attached :image
   validates :name, presence: true
   # Include default devise modules. Others available are:
